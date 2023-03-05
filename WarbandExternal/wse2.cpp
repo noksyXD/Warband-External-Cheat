5efb2b7f9548cc371bf42c10ef6823cf7bc6f81a
wayt <71033598+noksyXD@users.noreply.github.com>
2023-03-05 13:42:25 +0300

    Update wse2.cpp

--- WarbandExternal/wse2.cpp
+++ WarbandExternal/wse2.cpp
@@ -3,5 +3,5 @@ int otoblockws = 0;
 float zoomdistancews = 0;
 #include "wse2.h"
 void wse2::runWse2(HANDLE hProcessxx) {
-	// not supported but maybe can be support in next time
+	// not supported but maybe can support in next time
 }
